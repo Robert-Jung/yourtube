c0fa36aa6983c844e9c57fb6f7aee6f430fea1fc
Robert-Jung <rjinjung@gmail.com>
2017-06-07 01:19:45 -0700

    User can see list of searched videos

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+const API_KEY = 'AIzaSyCIEgledRFOL-N3VVAB3Aw3jb0FQ_A-llE'
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList
+          videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
